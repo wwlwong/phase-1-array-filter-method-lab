86665f4982d778906ae0d7a0fa475d62f535b3a6
William Wong <william.wai.lim.wong@gmail.com>
2023-04-02 13:41:40 -0400

    completed filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(driver, name){
+    return driver.filter(function (names) {
+        return names.toUpperCase() == name.toUpperCase();
+    });
+
+}
+
+function fuzzyMatch(driver, letter) {
+    return driver.filter(function (names) {
+        return names.startsWith(letter) == true;
+    });
+}
+
+function matchName(driver, string) {
+    let filterNames =  driver.filter(function(names) {
+        return names.name == string;
+      });
+    return filterNames;
+}
